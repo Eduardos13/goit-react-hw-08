d91933317cf5f245165d818b7e076b253e54f4cf
Eduardos13 <edik.kozelsky@gmail.com>
2024-08-13 22:57:35 -0400

    repo is ready
    all based repo is transferred from prev repo to this. all imports/exports are fixed and repo is ready to start work

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import ContactList from '../ContactList/ContactList';
+import ContactForm from '../ContactForm/ContactForm';
+import SearchBox from '../SearchBox/SearchBox';
+import { useDispatch } from 'react-redux';
+import { fetchContactsThunk } from '../../redux/contacts/operations';
 
 const App = () => {
-  return <div>App</div>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContactsThunk());
+  }, [dispatch]);
+  return (
+    <div>
+      <SearchBox />
+      <ContactForm />
+      <ContactList />
+    </div>
+  );
 };
 
 export default App;
