d64ab3a144cb29b174cf2ab6aa09c4da1e7470f9
Eduardos13 <edik.kozelsky@gmail.com>
2024-08-14 21:34:57 -0400

    extraReducers crearted

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { registerThunk } from './operations';
 
 const initialState = {
   user: {
@@ -12,6 +13,13 @@ const initialState = {
 const slice = createSlice({
   name: 'auth',
   initialState,
+  extraReducers: (builder) => {
+    builder.addCase(registerThunk.fulfilled, (state, acttion) => {
+      state.user = acttion.payload.user;
+      state.token = acttion.payload.token;
+      state.isLogedIn = true;
+    });
+  },
 });
 
 export const authReducer = slice.reducer;
